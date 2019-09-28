a5beea8c77bf0186e981ea8512219944117622f6
a-elnawawy <eng.ahmed.elnawawy@gmail.com>
2019-09-28 23:07:20 +0200

    make mohab the repo master

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <!-- startBoard -->
+  <section class="board">
+      <div class="container">
+        <div class="parent">
+          <user class="user"></user>
+          <user class="user"></user>
+          <user class="user"></user>
+          <user class="user"></user>
+          <user class="user"></user>
+          <user class="user"></user>
+          <user class="user"></user>
+        </div>
+      </div>
+  </section>
+  <!-- endBoard -->
+</template>
+
+<script>
+import user from './components/user.vue';
+export default {
+
+  components: {
+    'user': user
+  }
+
+}
+</script>
+
+<style>
+@import url('../css/variables.css');
+*{
+    padding: 0;
+    margin:0;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+}
+section.board{
+    background-color: var(--light-gray-color);
+}
+.container{
+  width:99%;
+  margin:auto;
+}
+.parent{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+
+}
+</style>
---
+++ src/components/board.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="box">
+    <user class="user"></user>
+  </div>
+
+</template>
+
+<script>
+import user from './components/user.vue'
+
+export default {
+  components: {
+    'user' : user
+  },
+  data(){
+    return{
+    }
+  }
+}
+</script>
+
+<style >
+.box{
+   display: flex;
+   width:99%;
+   margin: auto;
+   flex-wrap: wrap;
+   align-items: center;
+   justify-content: space-between;
+}
+.user{
+  background-color:red;
+  width:100px;
+  }
+</style>
